effe81cd3b5f8ab95c0efa55107a6620f7848db1
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-29 17:17:34 +0700

    final_1 up1

--- final_1.c
+++ final_1.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 int main ()
 {
-
+    int n,k,ch=0,i;
+    char c[2],ks[7];
+    scanf("%d %s",&n,c);
+    for(k=n; 1; k++)
+    {
+        sprintf(ks,"%d",k);
+        for(i=0; i<strlen(ks); i++)
+        {
+            if(ks[i]==c[0])
+            {
+                ch=1;
+            }
+        }
+    }
     return 0;
 }
