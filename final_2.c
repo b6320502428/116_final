570aa1b76d5df56be0407edf497a2c85a6861b84
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-29 17:25:50 +0700

    final_2 up1

--- final_2.c
+++ final_2.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 int main ()
 {
-    int n;
-
+    int n,i,max;
+    scanf("%d",&n);
+    int like[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&like[i]);
+    }
     return 0;
 }
