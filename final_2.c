25f8a9c5a5be6f2f8a88eb584d69e48c24af2c86
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-29 18:31:20 +0700

    final_2 up3
    Fin

--- final_2.c
+++ final_2.c
@@ -1,24 +1,40 @@
 #include <stdio.h>
 int main ()
 {
-    int n,i,max,c=1;
+    int n,i,max,c=1,only,tmp;
     scanf("%d",&n);
-    int like[n];
-    for(i=0;i<n;i++)
+    int like[10000]={0};
+    for(i=0; i<n; i++)
     {
-        scanf("%d",&like[i]);
+        scanf("%d",&tmp);
+        like[tmp-1]++;
     }
     max=like[0];
-    for(i=0;i<n;i++)
+    for(i=0; i<10000; i++)
     {
         if(max<like[i])
         {
             max=like[i];
             c=1;
+            only=i+1;
         }
         else if(max==like[i])
         {
             c++;
+        }
+    }
+    if(c==1)
+    {
+        printf("%d",only);
+    }
+    else
+    {
+        for(i=0; i<10000; i++)
+        {
+            if(max==like[i])
+            {
+                printf("%d ",i+1);
+            }
         }
     }
     return 0;
